4f19900b2ee8a6b54febd4a5afd493622edd8966
rudhravyas <91895312+rudhravyas@users.noreply.github.com>
2025-01-06 22:47:33 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const PORT = 5000;
 
 // Enable CORS for all origins or specify allowed origins
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app's URL
+  origin: 'http://localhost:5173','https://kicks-brown.vercel.app/Payment' // Your React app's URL
 }));
 
 // Middleware to parse JSON
